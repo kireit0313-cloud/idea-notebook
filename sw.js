1bf978777cb5bf56c067f583192ed6978dab8135
kireit0313-cloud <kireit0313@gmail.com>
2026-07-07 13:49:00 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'idea-loom-v9';
+const CACHE = 'idea-loom-v10';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
